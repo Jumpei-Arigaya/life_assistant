6102de937f49a95832eb9fe63af4627c9ac6195c
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-07-01 20:17:50 +0900

    バックエンドのディレクトリ構造を変更

--- back/api/shopping/apps.py
+++ back/api/shopping/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LifeAssistantConfig(AppConfig):
+class ShoppingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "life_assistant"
+    name = "api.shopping"
--- back/config/urls.py
+++ back/config/urls.py
@@ -1,4 +1,4 @@
-"""pj_life_assistant URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
